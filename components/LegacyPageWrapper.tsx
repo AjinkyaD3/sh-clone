da19862d94cc4bd4d0e7b04d21ca5f8df196177d
AjinkyaD3 <ajinkyadhotre201@gmail.com>
2026-09-08 22:00:37 +0530

    chk point 1

--- components/LegacyPageWrapper.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function LegacyPageWrapper({ html, bodyClass }: { html: string, bodyClass?: string }) {
-  return (
-    <div className={bodyClass} dangerouslySetInnerHTML={{ __html: html }} />
-  );
-}
--- scratch/test_dom_content.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const cheerio = require('cheerio');
-const html = fs.readFileSync('c:/Users/AJINKYA/OneDrive/Desktop/SH NEXT JS/secure-house-nextjs/app/content.html', 'utf-8');
-const $ = cheerio.load(html, { decodeEntities: false });
-console.log('header nodes:', $('.fusion-tb-header').length);
-console.log('footer nodes:', $('.fusion-tb-footer').length);
